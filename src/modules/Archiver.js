48152ff86f645a65a7adbc581b40b3d15f2c317f
Ec_@PC <dmatis01@gmail.com>
2019-01-22 18:50:06 +0100

    BUGFIX - Memory leak when file was too big

--- src/modules/Archiver.js
+++ src/modules/Archiver.js
@@ -1,18 +1,24 @@
 var tar = require('tar');
-var md5 = require('md5');
+var crypto = require('crypto');
 var fs = require('fs');
 
 class Archiver {
 
     static archive(folder, files, out) {
-        return tar.c({gzip: true, file: out, cwd: folder}, files);
+        return tar.c({ gzip: true, file: out, cwd: folder }, files);
     }
 
     static md5(file) {
-        return new Promise(function(resolve, reject) {
-            fs.readFile(file, function(err, buf) {
-                if(err) reject(err)
-                else resolve(md5(buf));
+        return new Promise(function (resolve, reject) {
+            var hash = crypto.createHash('md5'),
+                stream = fs.createReadStream(file);
+
+            stream.on('data', function (data) {
+                hash.update(data, 'utf8');
+            });
+
+            stream.on('end', function () {
+                resolve(hash.digest('hex'));
             });
         });
     }
